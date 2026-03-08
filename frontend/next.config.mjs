47f85bf89fcd1302c1d9e3a05c460dd70f4eff7b
𝕊𝕠𝕝𝕠𝕞𝕠𝕟 𝕆𝕓𝕚𝕒𝕝𝕠𝕣 <solomonobialor5@gmail.com>
2026-03-09 00:16:47 +0100

    Add webpack fallback configuration for specific modules

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,5 +6,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
 };
 export default nextConfig;
